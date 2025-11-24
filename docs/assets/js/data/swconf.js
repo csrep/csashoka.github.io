3c1c724f0ef80300461abdde2c2c2af918fd6f82
Ibrahim Khalil <ibrahimkhalil@Ibrahims-MacBook-Air-2.local>
2025-11-24 09:22:55 +0530

    manifest name changed

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1763339307',resources: [
+    cacheName: 'chirpy-1763956353',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
